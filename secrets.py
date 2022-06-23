f652ff29fc3f11a5560a4ca8e75954fbfc3bcfa1
Noslen pantaleon <69729849+noslenPantaleon@users.noreply.github.com>
2022-06-23 14:42:55 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,20 +1,18 @@
 secrets = {
     
-        'ssid':"lezica",
-        'password':"papicha4132",
-        'aio_username':"noslen",
-        'broker':"192.168.0.109",
+        'ssid':"add your wifi",
+        'password':"add your password",
+        'aio_username':"username",
+        'broker':"broker ip adress",
         'port':1883,
-        'username':'noslen',
-        'pass':'100loops',
-        'aio_key':'aio_qjZy19dVyq7fKyvow915cEah1dtL',
+        'username':'username',
+        'pass':'password',
+        'aio_key':'adfruit key',
         'timezone':"America/Argentina/Buenos_Aires",
            }
 
 
     
-    
-# ADAFRUIT_IO_USERNAME = "noslen"
-# ADAFRUIT_IO_KEY = "aio_qjZy19dVyq7fKyvow915cEah1dtL"
+
 
 
